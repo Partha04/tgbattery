49eb78309eba063249b2157be5a39e17cb2be547
PARTHA DEBNATH <you@example.com>
2019-09-07 23:25:40 +0530

    fixed horizontal scroll problem added dummy text

--- src/aboutus.js
+++ src/aboutus.js
@@ -6,13 +6,11 @@ import "./aboutus.css";
 import { aboutusinfo } from "./data/aboutusinfo";
 class Aboutus extends Component {
   state = {
-     aboutusinfo: aboutusinfo[0],
+    aboutusinfo: aboutusinfo[0]
   };
-  setdetails=(e)=>{
-    this.setState(
-      {aboutusinfo:aboutusinfo[e]}
-    )
-  }
+  setdetails = e => {
+    this.setState({ aboutusinfo: aboutusinfo[e] });
+  };
   render() {
     return (
       <div>
@@ -20,20 +18,40 @@ class Aboutus extends Component {
         <img className="dealerimg" src={dealernetwork} alt="dl" />
         <div class="outerb">
           <div class="sideabout">
-            <div class="sidetab" onClick={()=>this.setdetails(0)}>Company Details</div>
-            <div class="sidetab" onClick={()=>this.setdetails(1)} >Vision and Mission</div>
-            <div class="sidetab" onClick={()=>this.setdetails(2)}>Environmenal safty</div>
-            <div class="sidetab"onClick={()=>this.setdetails(3)}>Awards</div>
-            <div class="sidetab"onClick={()=>this.setdetails(4)}>Heritage</div>
+            <div class="sidetab" onClick={() => this.setdetails(0)}>
+              Company Details
+            </div>
+            <div class="sidetab" onClick={() => this.setdetails(1)}>
+              Vision and Mission
+            </div>
+            <div class="sidetab" onClick={() => this.setdetails(2)}>
+              Environmenal safty
+            </div>
+            <div class="sidetab" onClick={() => this.setdetails(3)}>
+              Awards
+            </div>
+            <div class="sidetab" onClick={() => this.setdetails(4)}>
+              Heritage
+            </div>
             <a href="contactus">
-
-            <div class="sidetab2">Locate Us</div>
+              <div class="sidetab2">Locate Us</div>
             </a>
           </div>
           <div class="aboutdetails">
             <h2>{this.state.aboutusinfo.heading}</h2>
 
-            <p>{this.state.aboutusinfo.details.map(para=>{return(<p>{para}</p>)})}</p>
+            <br />
+            <br />
+            <p>
+              {this.state.aboutusinfo.details.map(para => {
+                return (
+                  <div>
+                    <p>{para}</p>
+                    <br />
+                  </div>
+                );
+              })}
+            </p>
           </div>
         </div>
         <Footer />
